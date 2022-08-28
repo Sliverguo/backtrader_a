638e24f2f976c485851b8ebc0496fbca361222f5
sliver <sliver_ash@aliyun.com>
2022-08-28 13:37:25 +0800

    [Update] 修改tushare数据类支持bactraker所需格式

--- tushare_get_data.py
+++ tushare_get_data.py
@@ -1,5 +1,5 @@
 import tushare as ts
-
+import pandas as pd
 
 class tushare_data:
     pro = None
@@ -17,9 +17,21 @@ class tushare_data:
                          fields='ts_code,symbol,name,area,industry,fullname,enname,cnspell,market,exchange,curr_type,list_date')
         print(data)
 
+    def get_data_for_bt(self,code,start_date):
+        data = ts.pro_bar(ts_code=code, adj='hfq', start_date=start_date)
+        data.index = pd.to_datetime(data.trade_date)
+        data = data.sort_index()
+        data['volume'] = data.vol
+        data['openinterest'] = 0
+        data['datetime'] = pd.to_datetime(data.trade_date)
+        data = data[['datetime', 'open', 'high', 'low', 'close', 'volume', 'openinterest']]
+        data = data.fillna(0)
+        print(data)
+        return data
 
 if __name__ == '__main__':
     data = tushare_data('test','token')
     data.get_all_data()
     data.get_data_by_code('600089.SH')
+    data.get_data_for_bt('600089.SH','20220530')
 
