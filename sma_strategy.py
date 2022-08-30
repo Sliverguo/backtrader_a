265067f11b29e0a79fec871b13b09de1bef8079f
sliver <sliver_ash@aliyun.com>
2022-08-30 10:03:55 +0800

    [Update] 增加查找最佳参数类，找出最优购买仓位比

--- sma_strategy.py
+++ sma_strategy.py
@@ -14,7 +14,7 @@ class smastrategy(bt.Strategy):
         self.buyprice = None
         self.buycomm = None
 
-        self.sma = bt.indicators.MovingAverageSimple(self.data0, period=15)
+        self.sma = bt.indicators.MovingAverageSimple(self.data0, period=20)
 
     def next(self):
         # print("next")
@@ -61,20 +61,70 @@ class smastrategy(bt.Strategy):
             (trade.pnl, trade.pnlcomm)
         )
 
-    def log(self,txt, dt=None, doprint=True ):
+    def log(self,txt, dt=None, doprint=False):
         if doprint:
             dt = dt or self.datas[0].datetime.date(0)
             print('%s, %s' % (dt.isoformat(),txt))
 
 
+class get_best_parameter:
+    def __init__(self,dataframe,strategy):
+        self.result = []
+        self.cerebro = bt.Cerebro()
+        # feed pandas data
+        self.data_stock =bt.feeds.PandasData(dataname=dataframe, fromdate=dt.datetime(2018, 1, 1),
+                                  todate=dt.datetime(2022, 8, 29), timeframe=bt.TimeFrame.Days)
+        # add stock data into the cerebro
+        self.cerebro.adddata(self.data_stock)
+        # add strategy into the cerebro
+        self.cerebro.addstrategy(strategy)
+        # add analyzer into the cerebro
+        self.cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='SharpeRatio')
+        self.cerebro.addanalyzer(bt.analyzers.DrawDown, _name='DrawDown')
+        self.cerebro.addanalyzer(bt.analyzers.PositionsValue, _name='PositionsValue')
+
+    def set_cash_comm(self,cash,comm):
+        self.cerebro.broker.setcash(cash)
+        self.cerebro.broker.setcommission(commission=comm)
+
+    def get_best_resut(self):
+        big_value = 0.0
+        best_size = 1
+        for size in range(1,100):
+            self.cerebro.addsizer(bt.sizers.PercentSizer, percents=size)
+            result = self.cerebro.run()
+            self.result.append(result)
+            value_dict = result[0].analyzers.PositionsValue.get_analysis()
+            last_day_value = value_dict[(list(value_dict.keys())[-1])]
+            if(np.float_(last_day_value) > big_value):
+                big_value = np.float_(last_day_value)
+                best_size = size
+            # print('夏普比率：', result[0].analyzers.SharpeRatio.get_analysis()['sharperatio'])
+            # print('最大回撤：', result[0].analyzers.DrawDown.get_analysis()['max']['drawdown'])
+            # print('最终净值：', result[0].analyzers.PositionsValue.get_analysis()[dt.date(2022, 8, 29)])
+
+        print("最优的购买仓位比:%d" % best_size+"的最大净值是%f元" % big_value)
+        return self.result
+
+    def show(self):
+        self.cerebro.plot()
+
+
 if __name__ == '__main__':
-    cerebro = bt.Cerebro()
     data = tgd.tushare_data('bt', 'token')
     dataframe = data.get_data_as_pandas('600089.SH', '20180101')
+    gbp = get_best_parameter(dataframe,smastrategy)
+    gbp.set_cash_comm(200000,0.0005)
+    gbp.get_best_resut()
+    gbp.show()
+
+
+'''
+    # dataframe = data.get_data_as_pandas('300223.SZ', '20180101')
 
     print(type(dataframe))
-    data_tb = bt.feeds.PandasData(dataname=dataframe, fromdate=dt.datetime(2018, 1, 1),
-                                  todate=dt.datetime(2022, 8, 28), timeframe=bt.TimeFrame.Days)
+    data_tb = bt.feeds.PandasData(dataname=dataframe, fromdate=dt.datetime(2022, 1, 1),
+                                  todate=dt.datetime(2022, 8, 29), timeframe=bt.TimeFrame.Days)
     cerebro.adddata(data_tb)
 
     cerebro.addstrategy(smastrategy)
@@ -90,3 +140,4 @@ if __name__ == '__main__':
     print('夏普比率：',result[0].analyzers.SharpeRatio.get_analysis()['sharperatio'])
     print('最大回撤：',result[0].analyzers.DrawDown.get_analysis()['max']['drawdown'])
     cerebro.plot()
+'''
